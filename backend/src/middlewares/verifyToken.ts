0814455c5e41a5aea9c8ffcb97ef9d4807741012
Kira272921 <sunset272921@gmail.com>
2022-05-15 20:15:16 +0530

    fuck you anurag mf

--- backend/src/middlewares/verifyToken.ts
+++ backend/src/middlewares/verifyToken.ts
@@ -1,32 +1,8 @@
-import axios from "axios";
 import { Request, Response, NextFunction } from "express";
 
 const verifyToken = (req: Request, res: Response, next: NextFunction) => {
   if (req.headers["authorization"]?.split(" ")[1]) {
     next();
-    axios
-      .get("https://gated-solana-links.us.auth0.com/userinfo", {
-        headers: {
-          Authorization: `Bearer ${
-            req.headers["authorization"]?.split(" ")[1]
-          }`,
-        },
-      })
-      .then((response) => {
-        if (response.status === 401) {
-          return res.status(401).json({
-            message: "Unauthenticated",
-          });
-        } else {
-          next();
-        }
-      })
-      .catch((error) => {
-        return res.status(500).json({
-          message: "Internal Server Error",
-          error: error,
-        });
-      });
   } else {
     return res.status(401).json({
       message: "Unauthorized",
